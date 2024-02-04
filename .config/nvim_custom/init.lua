c4d61580d45ef9de510988b04be9e79812ab8c9b
Ujwal P <ujwalp09@gmail.com>
2024-02-04 01:00:31 -0800

    dotfiles: Add custom nvim conf

---
+++ .config/nvim_custom/init.lua
@@ -0,0 +1,24 @@
+-- local autocmd = vim.api.nvim_create_autocmd
+
+-- Auto resize panes when resizing nvim window
+-- autocmd("VimResized", {
+--   pattern = "*",
+--   command = "tabdo wincmd =",
+-- })
+vim.opt.colorcolumn = "80"
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.diagnostic.config({
+    virtual_text = false,
+    virtual_line = false,
+})
+vim.api.nvim_create_user_command("DiagToggle", function()
+	local config = vim.diagnostic.config
+	local vt = config().virtual_text
+	config {
+		virtual_text = not vt,
+		underline = not vt,
+		signs = not vt,
+	}
+end, { desc = "toggle diagnostic" })
