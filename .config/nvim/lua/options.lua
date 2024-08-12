e22d7f2cc9082a0d6d6b557d25a46acc819c2234
Ujwal P <ujwalp09@gmail.com>
2024-08-12 15:38:55 +0530

    dotfiles: Fix vim options

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -4,16 +4,16 @@ require "nvchad.options"
 
 local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
-o.opt.colorcolumn = "80"
-o.opt.tabstop = 4
-o.opt.softtabstop = 4
-o.opt.shiftwidth = 4
-o.diagnostic.config({
+o.colorcolumn = "80"
+o.tabstop = 4
+o.softtabstop = 4
+o.shiftwidth = 4
+vim.diagnostic.config({
     virtual_text = false,
     virtual_line = false,
 })
-o.api.nvim_create_user_command("DiagToggle", function()
-    local config = o.diagnostic.config
+vim.api.nvim_create_user_command("DiagToggle", function()
+    local config = vim.diagnostic.config
     local vt = config().virtual_text
     config {
         virtual_text = not vt,
